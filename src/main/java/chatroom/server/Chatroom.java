d6faca2c1158b2cc7aedbb459b5a5b16f6d6481e
Teodor Glisic <teodor.glisic@students.fhnw.ch>
2025-11-28 16:58:25 +0100

    Creation of Chatroom class

---
+++ src/main/java/chatroom/server/Chatroom.java
@@ -0,0 +1,47 @@
+package chatroom.server;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Chatroom {
+
+    private static final ArrayList<Chatroom> chatrooms = new ArrayList<>();
+    private final ArrayList<Client> members = new ArrayList<>();
+    private final Client user;
+    private final String chatroomName;
+
+
+    public Chatroom(Client user, String chatroomName) {
+        this.chatroomName = chatroomName;
+        chatrooms.add(this);
+        this.user = user;
+        members.add(user);
+    }
+
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || this.getClass() != o.getClass()) return false;
+        Chatroom chatroom = (Chatroom) o;
+        return this.chatroomName.equals(chatroom.getChatroomName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(chatroomName);
+    }
+
+    public String getChatroomName() {
+        return chatroomName;
+    }
+
+
+    public static List<Chatroom> listChatrooms() {
+        return chatrooms;
+    }
+
+}
